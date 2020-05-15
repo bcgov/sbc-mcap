b0ae5cb0e883af476cb94ffd9f8dcff594660f42
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-05-15 11:38:19 -0700

    Cleanup

--- src/examples/cobrowse/js/main.js
+++ src/examples/cobrowse/js/main.js
@@ -1,26 +1,26 @@
-//By waiting for document.ready, we know the previous two JS files have loaded. 
+// By waiting for document.ready, we know the previous two JS files have loaded.
 $(function (event) {
-    console.log("Page loaded")
-    addScript('https://video-poc1.maximusbc.ca:8443/assistserver/sdk/web/consumer/assist.js');
-    $('#cobrowse').on('click', initCobrowse);
+	console.log("Page loaded");
+	addScript("https://video-poc1.maximusbc.ca/assistserver/sdk/web/consumer/assist.js");
+	$("#cobrowse").on("click", initCobrowse);
 });
 
-//Load a remote JS file 
+// Load a remote JS file
 function addScript(url) {
-    console.log("adding script: ", url);
-    var element = document.createElement('script');
-    element.setAttribute('src', url);
-    document.head.appendChild(element);
+	console.log("adding script: ", url);
+	const element = document.createElement("script");
+	element.setAttribute("src", url);
+	document.head.appendChild(element);
 }
 
 function initCobrowse() {
-    console.log('initCobrowse called');
-    //isBrowserSupported is defined in assist.js, so we know it's loaded. 
-    if (AssistSDK.isBrowserSupported && AssistSDK.isBrowserSupported()) {
-        // These are set in appConfig.js
-        window.agentIdVar=appConfig.agentId;
-        window.videoAssistUrl=appConfig.videoAssistUrl;
-        AssistBoot.addAssistBehaviour();
-        AssistBoot.startAssistDialog();
-    }
-} 
+	console.log("initCobrowse called");
+	// isBrowserSupported is defined in assist.js, so we know it's loaded.
+	if (AssistSDK.isBrowserSupported && AssistSDK.isBrowserSupported()) {
+		// These are set in appConfig.js
+		window.agentIdVar = appConfig.agentId;
+		window.videoAssistUrl = appConfig.videoAssistUrl;
+		AssistBoot.addAssistBehaviour();
+		AssistBoot.startAssistDialog();
+	}
+}
--- src/examples/webchat/README.md
+++ src/examples/webchat/README.md
@@ -1,12 +1,12 @@
-### html webchat example
+# webchat example code
 
-This is a single file example for how to call webchat.
-You can run this from any  http server  or even as a local file.  It requires no Javascript for its basic operation although you may decide to add your own scripting for enhanced features
+### index.html
+
+This is a basic single file example for how to open a webchat session
+You can run this from any http server or even as a local file. It requires no Javascript for its basic operation although you may decide to add your own scripting for enhanced features.
+A webchat session must be started with an http POST request as shown in the code.  It is possible to wrap a POST in a GET as shown in the autopost example below.
 
 To use:
-
-1. Edit the "action" attribute of the form html element to match the specific webchat URL that you wish to connect to.
-2. open index.html with your browser.  For Windows computers, just double click on the file.
-3. Click the "Submit Query" button
-
-
+1. Edit the "action" attribute of the form element if needed.
+2. Open index.html with your browser.
+3. Click the "Start Webchat" button

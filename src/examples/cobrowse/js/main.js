bb3cdbf2fa366158faa1d0b02ec17b6f064a3141
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-05-15 13:08:51 -0700

    Comment

--- src/examples/cobrowse/js/main.js
+++ src/examples/cobrowse/js/main.js
@@ -7,7 +7,6 @@ const initCobrowse = function () {
 	console.log("initCobrowse called");
 	// AssistSDK is defined in assist.js, so we know it's loaded.
 	if (AssistSDK && AssistSDK.isBrowserSupported()) {
-		// These config items are set in appConfig.js
 		window.agentIdVar = appConfig.agentId;
 		window.videoAssistUrl = appConfig.videoAssistUrl;
 		AssistBoot.addAssistBehaviour();

9e07da540379e88c0a11143c2e53b9139dcb1351
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-05-15 12:32:25 -0700

    Removed unnecessary code

--- src/examples/cobrowse/js/main.js
+++ src/examples/cobrowse/js/main.js
@@ -1,26 +1,11 @@
-// By waiting for document.ready, we know the previous two JS files have loaded.
-$(function (event) {
-	console.log("Page loaded");
-	addScript("https://video-poc1.maximusbc.ca/assistserver/sdk/web/consumer/assist.js");
-	$("#cobrowse").on("click", initCobrowse);
-});
-
-// Load a remote JS file
-function addScript(url) {
-	console.log("adding script: ", url);
-	const element = document.createElement("script");
-	element.setAttribute("src", url);
-	document.head.appendChild(element);
-}
-
-function initCobrowse() {
+const initCobrowse = function () {
 	console.log("initCobrowse called");
 	// isBrowserSupported is defined in assist.js, so we know it's loaded.
 	if (AssistSDK.isBrowserSupported && AssistSDK.isBrowserSupported()) {
-		// These are set in appConfig.js
+		// These config items are set in appConfig.js
 		window.agentIdVar = appConfig.agentId;
 		window.videoAssistUrl = appConfig.videoAssistUrl;
 		AssistBoot.addAssistBehaviour();
 		AssistBoot.startAssistDialog();
 	}
-}
+};

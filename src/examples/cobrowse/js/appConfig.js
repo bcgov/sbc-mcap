f1fe5e0c7b1c6a4d05e29ef69d29378a4657d03f
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-05-15 13:02:04 -0700

    Moved config into main.js

--- src/examples/cobrowse/js/appConfig.js
+++
@@ -1,4 +0,0 @@
-const appConfig = {
-	videoAssistUrl: "https://video-poc1.maximusbc.ca",
-	agentId: "agent1"
-};
--- src/examples/cobrowse/js/main.js
+++ src/examples/cobrowse/js/main.js
@@ -1,3 +1,8 @@
+const appConfig = {
+	videoAssistUrl: "https://video-poc1.maximusbc.ca",
+	agentId: "agent1"
+};
+
 const initCobrowse = function () {
 	console.log("initCobrowse called");
 	// AssistSDK is defined in assist.js, so we know it's loaded.

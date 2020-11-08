4b9da346faeb6222969edc599b79ca89c91011c5
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-11-07 18:12:44 -0800

    Update

--- src/demo-spa/server.js
+++ src/demo-spa/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require('fs');
+const axios = require('axios');
 require('dotenv').config();
 /*=============================================================================
 All this node.js application does is save config items from the OS environment 
@@ -13,11 +13,17 @@ You don't even need that config file if you want to code in the config manually,
 as done in the /example files. They do not use the config file.
 =============================================================================*/
 const app = express();
+
+// Read the required items from environment. OS env overrides .env file
 const SERVICE_PORT = process.env.SERVICE_PORT || 8080;
+const SPA_ENV_URL = process.env.SPA_ENV_URL;
+const SPA_ENV_AUTH = process.env.SPA_ENV_AUTH;
+let spaConfig = {
+  SPA_ENV_MCAP_COBROWSE_URL: process.env.COBROWSE_URL,
+  SPA_ENV_MCAP_CHAT_SERVICES_URL: process.env.CHAT_SERVICES_URL
+};
 
-console.log(process.env.CHAT_SERVICES_URL);
-
-// This app just serves static file
+// These directories just serve static file
 app.use("/", express.static(__dirname + '/'));
 app.use("/js", express.static(__dirname + '/js'));
 app.use("/css", express.static(__dirname + '/css'));
@@ -27,16 +33,23 @@ app.use("/images", express.static(__dirname + '/images'));
 app.use("/webchat", express.static(__dirname + '/../examples/webchat'));
 app.use("/cobrowse", express.static(__dirname + '/../examples/cobrowse'));
 
-console.log(__dirname + '/../examples/webchat');
-
-// Read the required items from environment. OS env overrides .env file
-const configObj = {
-  cobrowse_url: cobrowse_url,
-  webchat_url: process.env.CHAT_SERVICES_URL,
-  spaenv_url: process.env.SPA_ENV_SERVER_URL,
-  agentId: process.env.AGENT_ID,
-  spaenv_auth: 'spaenv ' + process.env.SPA_ENV
+// Load environment from env server
+const names = '{"SPA_ENV_MCAP_COBROWSE_URL": "","SPA_ENV_MCAP_CHAT_SERVICES_URL": "" }';
+const headers = { 'Authorization': SPA_ENV_AUTH, 'SPA_ENV_NAME': names };
+let config = {
+  headers
 };
+console.log("Fetch from ENV_SERVER:", SPA_ENV_URL);
+axios.post(SPA_ENV_URL, null, config)
+  .then(res => {
+    console.log("status:", res.status);
+    spaConfig.SPA_ENV_MCAP_COBROWSE_URL = spaConfig.SPA_ENV_MCAP_COBROWSE_URL && process.env.COBROWSE_URL;
+    spaConfig.SPA_ENV_MCAP_CHAT_SERVICES_URL = spaConfig.SPA_ENV_MCAP_CHAT_SERVICES_URL && process.env.CHAT_SERVICES_URL;
+
+    console.log(spaConfig);
+  })
+  .catch(e => console.log(e.response.status, e.response.statusText, SPA_ENV_URL, config));
+  
 
 // used to send test/prod specific config to client
 app.get('/api/env', function (req, res) {

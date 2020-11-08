4b736c9429647a87cd7701a082f748988b5a58a1
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-11-07 19:39:46 -0800

    Update

--- src/demo-spa/js/index.js
+++ src/demo-spa/js/index.js
@@ -13,23 +13,25 @@ $(function () {
 
   // Fetch config
   $.get("/api/env", function (data) {
-    config = {...config, ...data};
+    config = { ...config, ...data };
     console.log(config);
     if (config.WEBCHAT_URL) {
       $(".chatpopup").attr("action", config.SPA_ENV_MCAP_CHAT_SERVICES_URL);
     }
+    initCoBrowse();
   });
 
-  $("#main-content .collapse").on("show.bs.collapse", onExpandSection);
+  $("#cobrowse_start").on("click", startCoBrowse);
+
+  $("#main-content .collapse").on("show.bs.collapse", expandSection);
 
   // Close collapsible sections when clicking outside.
   $("body").on("click", function () {
-    //Improvement: Only do for desktop? Check MOBILE_MAX_WIDTH?
     $("#main-content .collapse.in").collapse("hide");
+    console.log("collapse");
   });
 
   $("#main-content .collapse").on("click", function (e) {
-    console.log("dropdown body clicked");
     e.stopPropagation();
 
     // Don't stop event if it's form  fission
@@ -40,46 +42,43 @@ $(function () {
 
 });
 
-function readEnvironment() {
-  console.log("Loading Environment from ENV_SERVER");
+const startCoBrowse = function () {
+  Surfly.session().startLeader();
+};
 
-  var request = $.ajax({
-    type: "POST",
-    url: serverConfig.spaEnvServerURL,
-    processData: false,
-  });
+const initCoBrowse = function () {
+  (function (s, u, r, f, l, y) {
+    s[f] = s[f] || {
+      init: function () {
+        s[f].q = arguments;
+      },
+    };
+    l = u.createElement(r);
+    y = u.getElementsByTagName(r)[0];
+    l.async = 1;
+    l.src = config.COBROWSE_URL;
+    y.parentNode.insertBefore(l, y);
+  })(window, document, "script", "Surfly");
 
-  request.done(function (msg) {
-    // Comment these lines if you don't want this logged
-    console.log(msg);
-    console.log("Maintenance: " + msg.SPA_ENV_MCAP_MAINTENANCE_FLAG);
-    console.log("Chat services: " + msg.SPA_ENV_MCAP_CHAT_SERVICES_URL);
+  var settings = {
+    // Surfly session options can be set here, or at the Company/Plan levels.
+    widget_key: config.COBROWSE_KEY,
+    private_session: true, // to make sure only logged in agents can join the call
+    require_password: false,
+  };
 
-    // Note: Uncomment this if you want to disable after hours or maintenance mode
-    // If NOT in maintenance mode
-    // if (msg.SPA_ENV_MCAP_MAINTENANCE_FLAG == 'true') {
-    //     $("div#pagecontentid").hide();
-    //     $("div#maintenance").show();
-    //     $("#maintNotice").text(msg.SPA_ENV_MCAP_MAINTENANCE_MESSAGE);
-    // }
-    // else
-    {
-      assistjs_url = msg.SPA_ENV_MCAP_ASSISTJS_URL;
-      chatServicesUrl = msg.SPA_ENV_MCAP_CHAT_SERVICES_URL;
-      window.agentIdVar = msg.SPA_ENV_MCAP_AGENT_ID;
-      window.videoAssistUrl = msg.SPA_ENV_MCAP_VIDEO_ASSIST_URL;
-
-      // console.log('agent ID:' + window.agentIdVar + "----assist js url-------" + assistjs_url + '--chatServicesUrl:' + chatServicesUrl + '-- videoAssistUrl:' + window.videoAssistUrl);
-      $("div#maintenance").hide();
-      $("div#pagecontentid").show();
+  Surfly.init(settings, function (initResult) {
+    if (initResult.success) {
+      // API calls can now be made!
+      if (!Surfly.isInsideSession) {
+        Surfly.button();
+      }
+    } else {
+      console.log("Surfly was unable to initialize properly.");
     }
   });
 
-  request.fail(function (jqXHR, textStatus) {
-    console.log("Request failed: " + textStatus);
-    console.log("default values are assigned: ");
-  });
-}
+};
 
 function scrollTo($el, scrollTime) {
   if (!scrollTime) scrollTime = 500;
@@ -93,24 +92,29 @@ function scrollTo($el, scrollTime) {
   }, 250);
 }
 
-function onExpandSection() {
+function expandSection() {
+
+  console.log($(this).attr('id'));
+
+  // console.log($(this).children());
+
   //1. Make sure it's full-width (for desktop breakpoints)
   //2. Make sure it starts at the left (matching #main-content)
   // console.log("expand");
 
-  //Close all others
+  // Close all others
   if ($("#main-content .collapse.in").length) {
     $("#main-content .collapse").collapse("hide");
   }
 
-  //Full width
+  // Full width
   var position = $("#main-content").position().left - $(this).position().left;
   var position = position + 45; //account for #main-content padding
   var baseWidth = parseInt($("#main-content").css("width"), 10);
   var width = baseWidth - position;
   $(this).css("width", width);
 
-  //Start on left matching #main-content
+  // Start on left matching #main-content
   if ($(window).width() > MOBILE_MAX_WIDTH) {
     var mainOffset = $("#main-content").offset().left;
     // let dataParent = $(this).attr('data-parent');

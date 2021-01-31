d31d6afe14b579f6809600698fcd2267a3540a91
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-01-31 06:28:56 -0800

    Update

--- src/examples/schedule/index.js
+++ src/examples/schedule/index.js
@@ -2,21 +2,21 @@ $(document).ready(() => {
   $("#statusButton").on("click", showStatus);
 });
 
-const BASEURL = "https://mcap-schedule-3a0694-test.apps.silver.devops.gov.bc.ca/api/status/";
+const BASEURL =
+  "https://mcap-schedule-3a0694-test.apps.silver.devops.gov.bc.ca/api/status/";
 const BASELOCAL = "http://localhost:8080/api/status/";
 
 const showStatus = function () {
   const code = $("#code").val();
-  const url = BASELOCAL + code;
+  const url = BASEURL + code;
 
-  console.log(url);
-
-  $.ajax({
-    url,
-    dataType: "json",
-    success: function (result) {
-      console.log(result);
+  $.ajax({ url, dataType: "json" })
+    .done(result => {
+      console.log("then", result);
       $("#statusText").html(result.status);
-    },
-  });
+    })
+    .fail((err) => {
+      console.log(err.status);
+      $("#statusText").html("Error");
+    });
 };

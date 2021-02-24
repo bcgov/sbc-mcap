5f5a46a44af52b2aa72c4d6ed4e035ccb46bbf4e
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-02-23 19:13:24 -0800

    Update Demo

--- src/examples/cobrowse/js/cobrowse.js
+++ src/examples/cobrowse/js/cobrowse.js
@@ -9,7 +9,7 @@
   y = u.getElementsByTagName(r)[0];
   l.async = 1;
   // l.src = "https://t1cobrowse.maximusbc.ca/surfly.js";
-  l.src = "'https://cobrowse.maximusbc.ca/surfly.js'";
+  l.src = "https://cobrowse.maximusbc.ca/surfly.js";
   y.parentNode.insertBefore(l, y);
 })(window, document, "script", "Surfly");
 

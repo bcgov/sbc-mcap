141784adbc2064334540a1e30f567ef9572f9300
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-02-24 21:40:54 -0800

    Move test & prod params to top

--- src/examples/cobrowse/js/cobrowse.js
+++ src/examples/cobrowse/js/cobrowse.js
@@ -1,4 +1,12 @@
 
+//  Test Environment
+const url = "https://t1cobrowse.maximusbc.ca/surfly.js";
+const widget_key = "d37355ecd9714593bbefbcf498e58206";
+
+//  Prod Environment
+// const widget_key = "e6da7b3cf6c94e7a8a8a0674a0cd14f5"; // prod
+// const url = "https://cobrowse.maximusbc.ca/surfly.js";
+
 (function (s, u, r, f, l, y) {
   s[f] = s[f] || {
     init: function () {
@@ -8,14 +16,13 @@
   l = u.createElement(r);
   y = u.getElementsByTagName(r)[0];
   l.async = 1;
-  l.src = "https://t1cobrowse.maximusbc.ca/surfly.js";
-  // l.src = "https://cobrowse.maximusbc.ca/surfly.js";
+  l.src = url;
   y.parentNode.insertBefore(l, y);
 })(window, document, "script", "Surfly");
 
 const settings = {
   // Surfly session options can be set here, or at the Company/Plan levels.
-  widget_key: "e6da7b3cf6c94e7a8a8a0674a0cd14f5",
+  widget_key,
   private_session: true, // to make sure only logged in agents can join the call
   require_password: false,
 };
